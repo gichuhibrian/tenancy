45cfc4cabbe5ae82dc2d5ee32a5096de059326dd
Brian Chege <brianchegegichuhi@gmail.com>
2022-02-16 10:49:07 +0300

    setup for multitenancy

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::domain('{tenant}.localhost')->middleware('tenant')->group(function () {
+    Route::get('/', function ($tenant) {
+        return view('welcome');
+    });
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
